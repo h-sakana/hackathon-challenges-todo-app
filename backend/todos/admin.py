afc31c68e1808b8aed0674459fe4c8d327e577bd
h-sakana <h.sakana.prv@gmail.com>
2026-06-17 22:44:07 +0900

    管理画面の表示をカスタム

--- backend/todos/admin.py
+++ backend/todos/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Todo
+from .models import User, Todo
 
-admin.site.register(Todo)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "password", "created_at", "last_login_at")
+    list_filter = ("name", "created_at")
+    search_fields = ("name",)
 
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_checked", "created_at", "updated_at", "deleted_at")
+    list_filter = ("user", "is_checked",)
+    search_fields = ("name",)
