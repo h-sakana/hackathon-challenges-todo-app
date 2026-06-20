497d8bd0285ef82901a7c2504c2946f797ba4339
h-sakana <h.sakana.prv@gmail.com>
2026-06-20 16:20:14 +0900

    実機のレイアウト崩れ対応

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -55,12 +55,14 @@ export const LoginPage = () => {
 
       navigate("/todos");
 
-      // スマホ実機のレイアウト崩れ対応
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "instant",
-      });
+      // スマホ実機のレイアウト崩れ対策
+      setTimeout(() => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "instant",
+        });
+      }, 0);
     } catch (error) {
       console.error(error);
       alert(
